6d53f49ddc2a38fc47904cee8e638795ecc31ec6
KovalM <michail.kovalev7@gmail.com>
2024-08-09 20:56:29 +0300

    Add submodules

---
+++ problem-solver/sc-machine/sc-tools/sc-builder/src/builder_defines.hpp
@@ -0,0 +1,1 @@
+#define SC_PREPARE_BUILD_SCRIPTS_PATH "/home/mkovalev/job/techpark/fridman/problem-solver/sc-machine/scripts"
---
+++ problem-solver/sc-machine/sc-tools/sc-server/tests/test_defines.hpp
@@ -0,0 +1,2 @@
+#define SC_SERVER_REPO_PATH "/home/mkovalev/job/techpark/fridman/cmake-build-debug/../bin/sc-server-test-repo"
+#define SC_SERVER_INI "/home/mkovalev/job/techpark/fridman/problem-solver/sc-machine/sc-tools/sc-server/tests/sc-server-test.ini"
