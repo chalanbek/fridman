a676b36063ddd48d6aed85ecab13097d52d63602
Vaffi528 <trukhalex8@gmail.com>
2024-09-15 21:27:12 +0300

    feat: starter descripting the agent check problem solution answer

--- problem-solver/py/modules/messageProcessingModule/MessageProcessingModule.py
+++ problem-solver/py/modules/messageProcessingModule/MessageProcessingModule.py
@@ -1,7 +1,9 @@
 from sc_kpm import ScModule
 from .WeatherAgent import WeatherAgent
 from .AgentGetProblemText import AgentGetProblemText
+from .AgentCheckProblemSolutionAnswer import AgentCheckProblemSolutionAnswer
+
 
 class MessageProcessingModule(ScModule):
     def __init__(self):
-        super().__init__(WeatherAgent(), AgentGetProblemText())
+        super().__init__(WeatherAgent(), AgentGetProblemText(), AgentCheckProblemSolutionAnswer())
