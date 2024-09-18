5a39c1bf0822d85dfb1b4c8b8155576cd5ffc0cb
chalandbek <netrogayandreyaa@gmail.com>
2024-09-18 20:12:11 +0300

    feat: started AgentUpdateUserKnowledgeLevel

---
+++ problem-solver/py/modules/messageProcessingModule/AgentUpdateUserKnowledgeLevel.py
@@ -0,0 +1,134 @@
+"""
+This code creates some test agent and registers until the user stops the process.
+For this we wait for SIGINT.
+"""
+import logging
+from sc_client.models import ScAddr, ScLinkContentType, ScTemplate
+from sc_client.constants import sc_types
+from sc_client.client import template_search, get_links_by_content
+
+from sc_kpm import ScAgentClassic, ScModule, ScResult, ScServer
+from sc_kpm.sc_sets import ScSet
+from sc_kpm.utils import (
+    create_link,
+    get_link_content_data,
+    check_edge, create_edge,
+    delete_edges,
+    get_element_by_role_relation,
+    get_element_by_norole_relation,
+    get_system_idtf,
+    get_edge
+)
+from sc_kpm.utils.action_utils import (
+    create_action_answer,
+    finish_action_with_status,
+    get_action_arguments,
+    get_element_by_role_relation
+)
+from sc_kpm import ScKeynodes
+
+import requests
+
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s | %(name)s | %(message)s", datefmt="[%d-%b-%y %H:%M:%S]"
+)
+
+
+class AgentUpdateUserKnowledgeLevel(ScAgentClassic):
+    def __init__(self):
+        super().__init__("action_update_user_knowledge_level")
+
+    def on_event(self, event_element: ScAddr, event_edge: ScAddr, action_element: ScAddr) -> ScResult:
+        result = self.run(action_element)
+        is_successful = result == ScResult.OK
+        finish_action_with_status(action_element, is_successful)
+        self.logger.info("AgentUpdateUserKnowledgeLevel finished %s",
+                         "successfully" if is_successful else "unsuccessfully")
+        return result
+
+    def run(self, action_node: ScAddr) -> ScResult:
+        self.logger.info("AgentUpdateUserKnowledgeLevel started")
+
+        try:
+            user_addr = get_action_arguments(action_node, 1)[0]
+            task_addr = get_action_arguments(action_node, 2)[0]
+            if not user_addr.is_valid():
+                self.logger.error('AgentUpdateUserKnowledgeLevel: there are no argument with user')
+                return ScResult.ERROR
+            if not task_addr.is_valid():
+                self.logger.error('AgentUpdateUserKnowledgeLevel: there are no argument with task')
+                return ScResult.ERROR
+            
+            nrel_user = ScKeynodes.resolve('nrel_user', sc_types.NODE_CONST_NOROLE)
+            nrel_level_of_knowledge_of_topic = ScKeynodes.resolve('nrel_level_of_knowledge_of_topic', sc_types.NODE_CONST_NOROLE)
+
+            template = ScTemplate()
+            template.triple_with_relation(
+                user_addr, 
+                sc_types.EDGE_D_COMMON_VAR,
+                (sc_types.EDGE_D_COMMON_VAR, '_pair_theme_level'),
+                sc_types.EDGE_ACCESS_VAR_POS_PERM,
+                nrel_level_of_knowledge_of_topic
+            )
+
+            results = template_search(template)
+            result = results[0]
+            pair_theme_level_addr = result.get('_pair_theme_level')
+
+            template = ScTemplate()
+            template.triple_with_relation (
+                (sc_types.NODE_VAR, '_topic')
+                sc_types.EDGE_ACCESS_VAR_POS_PERM,
+                task_addr
+                sc_types.EDGE_ACCESS_VAR_POS_PERM,
+                rrel_problem_topic
+            )
+
+            results = template_search(template)
+            result = results[0]
+            topic_addr = result.get('_topic')
+
+            template = ScTemplate()
+            template.triple(
+                topic_addr
+                pair_theme_level_addr
+                (sc_types.NODE_VAR, '_knowledge_level')
+            )
+            results = template_search(template) 
+            result = results[0]
+            knowledge_level_link = result.get('_knowledge_level')
+            knowledge_level = get_link_content_data(knowledge_level_link)
+
+            template = ScTemplate()
+            template.triple_with_relation(
+                task_addr
+                sc_types.EDGE_D_COMMON_VAR,
+                (sc_types.LINK_VAR, '_complexity_level'),
+            )
+
+            results = template_search(template)
+            result = results[0]
+            complexity_level_link = result.get('_complexity_level')
+            complexity_level = get_link_content_data(complexity_level_link)
+            task_count_coefficient = 20
+            knowledge_level_coefficient = 2
+            knowledge_level_rounded = round(knowledge_level)
+            
+
+
+            """
+            knowledge_level = knowledge_level + (1/task_count_coefficient)*(knowledge_level_coefficient**(complexity_level-knowledge_level_rounded))
+            knowledge_level = knowledge_level + (1/task_count_coefficient)*(knowledge_level_coefficient**(knowledge_level_rounded-complexity_level))
+
+            """
+            
+            link_content2 = ScLinkContent(knowledge_level, ScLinkContentType.FLOAT, knowledge_level_link)
+            status = set_link_contents(link_content2)
+
+        except Exception as e:
+            self.logger.info(f"AgentUpdateUserKnowledgeLevel: finished with an error {e}")
+            return ScResult.ERROR
+
+        return ScResult.OK
+        
