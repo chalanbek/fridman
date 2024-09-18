295cdc4e073089dde2c2f7dab2a9e9762f2b09b4
Vaffi528 <trukhalex8@gmail.com>
2024-09-18 20:31:50 +0300

    feat: started descripting agent update user knowledge level, continue descripting agent check problem solution

--- problem-solver/py/modules/messageProcessingModule/MessageProcessingModule.py
+++ problem-solver/py/modules/messageProcessingModule/MessageProcessingModule.py
@@ -2,8 +2,10 @@ from sc_kpm import ScModule
 from .WeatherAgent import WeatherAgent
 from .AgentGetProblemText import AgentGetProblemText
 from .AgentCheckProblemSolutionAnswer import AgentCheckProblemSolutionAnswer
+from .AgentUpdateUserKnowledgeLevel import AgentUpdateUserKnowledgeLevel
+
 
 
 class MessageProcessingModule(ScModule):
     def __init__(self):
-        super().__init__(WeatherAgent(), AgentGetProblemText(), AgentCheckProblemSolutionAnswer())
+        super().__init__(WeatherAgent(), AgentGetProblemText(), AgentCheckProblemSolutionAnswer(), AgentUpdateUserKnowledgeLevel())
