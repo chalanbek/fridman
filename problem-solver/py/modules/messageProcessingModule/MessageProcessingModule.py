4abb211c94c727433fbe198a465dc1a9e9302ca1
Vaffi528 <trukhalex8@gmail.com>
2024-09-28 20:57:03 +0300

    feat: added agent get complete solution

--- problem-solver/py/modules/messageProcessingModule/MessageProcessingModule.py
+++ problem-solver/py/modules/messageProcessingModule/MessageProcessingModule.py
@@ -3,9 +3,10 @@ from .WeatherAgent import WeatherAgent
 from .AgentGetProblemText import AgentGetProblemText
 from .AgentCheckProblemSolutionAnswer import AgentCheckProblemSolutionAnswer
 from .AgentUpdateUserKnowledgeLevel import AgentUpdateUserKnowledgeLevel
-
-
+from .AgentGetHint import AgentGetHint
+from .AgentGetCompleteSolution import AgentGetCompleteSolution
+from .AgentGetShortSolution import AgentGetShortSolution
 
 class MessageProcessingModule(ScModule):
     def __init__(self):
-        super().__init__(WeatherAgent(), AgentGetProblemText(), AgentCheckProblemSolutionAnswer(), AgentUpdateUserKnowledgeLevel())
+        super().__init__(WeatherAgent(), AgentGetProblemText(), AgentCheckProblemSolutionAnswer(), AgentUpdateUserKnowledgeLevel(), AgentGetHint(), AgentGetCompleteSolution(), AgentGetShortSolution())
